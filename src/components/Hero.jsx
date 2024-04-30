e1213c789f33e2932aca86521b63d6e88067fe05
Soumyadeep <deepsoumya442@gmail.com>
2024-05-01 00:38:18 +0530

    Update resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import styles from './Hero.module.css'
-import photo from '../assets/Photo.jpg';
+import photo from '../assets/Photo5.jpg';
+import cv from "../assets/Soumyadeep_resume.pdf"
 const Hero = () => {
   return (
     <section className={styles.section}>
-        <div className={styles.content}>
+      <div className={styles.content}>
         <h1 className={styles.heading}>Hi,i am Soumyadeep</h1>
         <p className={styles.p}>I am a passionate full stack Software Developer.Reach out me if you want to know more!</p>
-        <a href="mailto:deepsoumya442@gmail.com" className={styles.contactBtn}>Contact Me</a>      
+       <div className={styles.aboutButton}>
+        
+        <a href="mailto:deepsoumya442@gmail.com" className={styles.contactBtn}>Contact Me</a>   
+        <a href={cv} target="_blank">
+        <button className={styles.cv}>Check Resume</button>
+        </a>
+        </div>
         </div>
         <div className={styles.div}>
         <img className={styles.img} src={photo} alt="Photo"></img>
